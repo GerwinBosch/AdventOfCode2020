9b2f3573ca93588253de2f86c9d44374f24fc84e
gerwinbosch <gerwinbosch@chello.nl>
2020-12-03 17:23:50 +0100

    Feat: Day 3

---
+++ day 3/pather.py
@@ -0,0 +1,26 @@
+right = 0
+treeCounter = 0
+firstTask = False
+
+def goSki(stepsizeLeft, stepsizeDown) :
+    right = 0
+    treeCounter = 0
+    with open("./forest.txt") as fp:
+        line = fp.readline().rstrip()
+        while line :
+            if line[right % len(line)] == "#":
+                treeCounter+=1
+            right +=stepsizeLeft
+            goDown = stepsizeDown
+            while goDown != 1:
+                fp.readline()
+                goDown-=1
+            line = fp.readline().rstrip()
+    return treeCounter
+
+firstResult = goSki(3,1)
+print("First task:", firstResult)
+
+
+
+print("Second task:", goSki(1,1) * firstResult * goSki(5,1) * goSki(7,1) * goSki(1,2))
